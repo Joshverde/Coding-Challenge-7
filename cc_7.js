ed34556a32c6b8dc43d4834a704c9dcf269e3d5d
Joshverde <52259302+Joshverde@users.noreply.github.com>
2025-02-16 15:55:10 -0500

    Task 2 -Employee Hourly Wage Calculation

--- cc_7.js
+++ cc_7.js
@@ -6,3 +6,12 @@ function calculateInvoice(subtotal,taxRate,discount){ // creates the function an
 console.log("**Task 1**"); // for ease of readabillity in the console
 calculateInvoice(100,0.08,5); // calls function
 calculateInvoice(500,0.1,20); // calls function
+
+//Task 2 -Employee Hourly Wage Calculation
+function calculateHourlyWage(salary,hoursPerWeek){ // creates the function and parameters
+    let hourlyWage = salary/(hoursPerWeek*52); // calculates the hourly wage
+    console.log(`Hourly wage: $${hourlyWage.toFixed(2)}`);
+};
+console.log("**Task 2**"); // for ease of readabillity in the console
+calculateHourlyWage(52000,40); // calls the function
+calculateHourlyWage(75000,35); // calls the function
